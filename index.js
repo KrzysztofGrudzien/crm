d48579167747064adcfd9460294d2215693de95c
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-28 13:42:34 +0100

    install express, handlebars and create basic configuration

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('ok');
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const app = express();
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.static( 'public'));
+app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.listen(3000, 'localhost', () => {
+    console.log('Listening on port 3000')
+});
