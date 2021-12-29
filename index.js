3bb03f926f75ad8fe026e5225f30fa4e9504ee77
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-29 01:04:00 +0100

    redirect resources from global to the client router using home router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {clientRouter}  = require("./routers/client");
+const {homeRouter}  = require("./routers/home");
 const express = require('express');
 const hbs = require('express-handlebars');
 
@@ -10,6 +11,7 @@ app.use(express.static( 'public'));
 app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.use('/client', clientRouter);
+app.use('/', homeRouter);
 app.listen(3000, 'localhost', () => {
     console.log('Listening on port 3000')
 });
